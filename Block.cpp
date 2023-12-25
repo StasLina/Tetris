194dea6cff84183adea4d537cec50e1a79f90b0c
BuildTools <unconfigured@null.spigotmc.org>
2023-12-25 08:06:52 +0300

    Приведение к стилю кода

--- Block.cpp
+++ Block.cpp
@@ -1,121 +1,148 @@
 #include "Block.h"
 
-const ModelIndex& BlockModel::GetModelIndex(const short& pos_x, const short& pos_y) {
-    std::vector<ModelIndex>::iterator elm;
-    auto pair=GetItemPosition(pos_x, pos_y);
-    short test_x = pair.first;
-    short test_y = pair.second;
-    auto row = data_model.begin();
-    int n;
-    for (n = 0; n != test_y; ++n) {
-        ++row;
-    }
-    elm = row->begin();
-    for (n = 0; n != test_x; ++n) {
-        ++elm;
-    }
-    return *elm;
+const ModelIndex &BlockModel::GetModelIndex(const short &pos_x,
+                                            const short &pos_y) {
+  std::vector<ModelIndex>::iterator elm;
+  auto pair = GetItemPosition(pos_x, pos_y);
+  short test_x = pair.first;
+  short test_y = pair.second;
+  auto row = data_model.begin();
+  int n;
+
+  for (n = 0; n != test_y; ++n) {
+    ++row;
+  }
+
+  elm = row->begin();
+
+  for (n = 0; n != test_x; ++n) {
+    ++elm;
+  }
+
+  return *elm;
 }
 
-void BlockModel::SetModelIndex(const ModelIndex& smth_data, const short& pos_x, const short& pos_y) {
-    auto row = data_model.begin();
-    int n;
-    for (n = 0; n != pos_y; ++n) {
-        ++row;
-    }
-    auto elm = row->begin();
-    for (n = 0; n != pos_x; ++n) {
-        ++elm;
-    }
-    *elm = smth_data;
-    return;
+void BlockModel::SetModelIndex(const ModelIndex &smth_data,
+                               const short &pos_x,
+                               const short &pos_y) {
+  auto row = data_model.begin();
+  int n;
+
+  for (n = 0; n != pos_y; ++n) {
+    ++row;
+  }
+  auto elm = row->begin();
+
+  for (n = 0; n != pos_x; ++n) {
+    ++elm;
+  }
+  *elm = smth_data;
+
+  return;
 }
 
-void BlockModel::SetBlokShape(const std::string& smth_str) {
-    int cur_row = 0, cur_col = 0;
-    auto smth_char = smth_str.begin();
-    auto smth_char_end = smth_str.end();
+void BlockModel::SetBlokShape(const std::string &smth_str) {
+  int cur_row = 0, cur_col = 0;
+  auto smth_char = smth_str.begin();
+  auto smth_char_end = smth_str.end();
 
-    //определяем размер
-    while (smth_char != smth_char_end) {
-        ++cur_col;
-        if (*smth_char == '#') {
-            if (max_col < cur_col) {
-                max_col = cur_col;
-            }
-            if (min_col > cur_col) {
-                min_col = cur_col;
-            }
-        }
-        if (*smth_char == '\n') {
-            //new line
-            ++cur_row;
-            if (max_row < cur_row) {
-                max_row = cur_row;
-            }
-            if (min_row > cur_row) {
-                min_row = cur_row;
-            }
-            cur_col = 0;
-        }
-        ++smth_char;
+  // определяем размер
+  while (smth_char != smth_char_end) {
+    ++cur_col;
+
+    if (*smth_char == '#') {
+
+      if (max_col < cur_col) {
+        max_col = cur_col;
+      }
+
+      if (min_col > cur_col) {
+        min_col = cur_col;
+      }
     }
-    this->SetModelSize(max_col, max_row);
 
-    smth_char = smth_str.begin();
-    cur_col = 0;
-    cur_row = 0;
+    if (*smth_char == '\n') {
+      // new line
+      ++cur_row;
 
-    while (smth_char != smth_char_end) {
-        switch (*smth_char) {
-        case '#':
-            SetModelIndex('#', cur_col, cur_row);
-            break;
-        case '\n':
-            cur_col = -1;
-            ++cur_row;
-            break;
-        default:
-            break;
-        }
-        ++cur_col;
-        ++smth_char;
+      if (max_row < cur_row) {
+        max_row = cur_row;
+      }
+
+      if (min_row > cur_row) {
+        min_row = cur_row;
+      }
+      cur_col = 0;
     }
-    //cout_matr();
+    ++smth_char;
+  }
+  this->SetModelSize(max_col, max_row);
+
+  smth_char = smth_str.begin();
+  cur_col = 0;
+  cur_row = 0;
+
+  while (smth_char != smth_char_end) {
+    switch (*smth_char) {
+    case '#':
+      SetModelIndex('#', cur_col, cur_row);
+      break;
+    case '\n':
+      cur_col = -1;
+      ++cur_row;
+      break;
+    default:
+      break;
+    }
+    ++cur_col;
+    ++smth_char;
+  }
+  // cout_matr();
 }
 
-inline std::pair<short, short> BlockModel::GetItemPosition(const short&x,const short &y) {
-    switch (this->cur_rotation) {
-    case AbstractBlockModel::types_rotation::rotate_0:
-        return { x,y };
-    case AbstractBlockModel::types_rotation::rotate_90:
-        return { y,max_row-x-1 };
-    case AbstractBlockModel::types_rotation::rotate_180:
-        return { max_col - x - 1, max_row - y - 1 };
-    case AbstractBlockModel::types_rotation::rotate_270:
-        return { max_col - y - 1, x };
-    }
-    /*case AbstractBlockModel::types_rotation::rotate_0:
-        return { x,y };
-    case AbstractBlockModel::types_rotation::rotate_90:
-        return { y,x };
-    case AbstractBlockModel::types_rotation::rotate_180:
-        return { max_col - x - 1, max_row - y - 1 };
-    case AbstractBlockModel::types_rotation::rotate_270:
-        return { max_col - y - 1, max_row - x - 1 };
-    }*/
+inline std::pair<short, short>
+BlockModel::GetItemPosition(const short &x, const short &y) {
+  switch (this->cur_rotation) {
+  case AbstractBlockModel::types_rotation::rotate_0:
+
+    return {x, y};
+  case AbstractBlockModel::types_rotation::rotate_90:
+
+    return {y, max_row - x - 1};
+  case AbstractBlockModel::types_rotation::rotate_180:
+
+    return {max_col - x - 1, max_row - y - 1};
+  case AbstractBlockModel::types_rotation::rotate_270:
+
+    return {max_col - y - 1, x};
+  }
+  /*case AbstractBlockModel::types_rotation::rotate_0:
+      
+return { x,y };
+  case AbstractBlockModel::types_rotation::rotate_90:
+      
+return { y,x };
+  case AbstractBlockModel::types_rotation::rotate_180:
+      
+return { max_col - x - 1, max_row - y - 1 };
+  case AbstractBlockModel::types_rotation::rotate_270:
+      
+return { max_col - y - 1, max_row - x - 1 };
+  }*/
 }
 
-void BlockModel::SetModelSize(unsigned short col_count, unsigned short row_count) {
-    data_model.resize(row_count);
-    auto smth_row = data_model.begin();
-    auto smth_row_end = data_model.end();
-    while (smth_row != smth_row_end) {
-        smth_row->resize(col_count);
-        ++smth_row;
-    }
+void BlockModel::SetModelSize(unsigned short col_count,
+                              unsigned short row_count) {
+  data_model.resize(row_count);
+  auto smth_row = data_model.begin();
+  auto smth_row_end = data_model.end();
+
+  while (smth_row != smth_row_end) {
+    smth_row->resize(col_count);
+    ++smth_row;
+  }
 }
 
-void BlockModel::SetParent(AbstractModel* parent) {
-    this->parent = parent;
+void BlockModel::SetParent(AbstractModel *parent) {
+  this->parent = parent;
 }
--- main.cpp
+++ main.cpp
@@ -1,165 +1,168 @@
-﻿#include <iostream>
-#include <map>
+﻿// clang-format.exe -i -style=file -files=files_for_format
 
 #include "Block.h"
 #include "map.h"
+
 #include <conio.h>
+#include <iostream>
+#include <map>
+
 int GlobalVars::x_size = 10;
 int GlobalVars::y_size = 10;
 bool GlobalVars::need_new_block = true;
-std::map<int, AbstractModel*> AbstractModel::list_bloks;
+std::map<int, AbstractModel *> AbstractModel::list_bloks;
 short GlobalVars::wide_scale = 2;
 
 int loadBloks() {
-    BlockModel* block;
-    block = new BlockModel(
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-    );
-    block = new BlockModel(
-        "#\n"
-        "#\n"
-        "##\n"
-    );
-    block = new BlockModel(
-        " #\n"
-        " #\n"
-        "##\n"
-    );
-    block = new BlockModel(
-        "##\n"
-        "##\n"
-    );
-    block = new BlockModel(
-        " ##\n"
-        "##\n" 
-    );
-    block = new BlockModel(
-        "##\n"
-        " ##\n"
-    );
-    block = new BlockModel(
-        "###\n"
-        " #\n"
-    );
-   /* 
-    block = new BlockModel(
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-    );
-    block = new BlockModel(
-        "##\n"
-        "# \n"
-    );
-    block= new BlockModel(
+  BlockModel *block;
+  block = new BlockModel("#\n"
+                         "#\n"
+                         "#\n"
+                         "#\n");
+  block = new BlockModel("#\n"
+                         "#\n"
+                         "##\n");
+  block = new BlockModel(" #\n"
+                         " #\n"
+                         "##\n");
+  block = new BlockModel("##\n"
+                         "##\n");
+  block = new BlockModel(" ##\n"
+                         "##\n");
+  block = new BlockModel("##\n"
+                         " ##\n");
+  block = new BlockModel("###\n"
+                         " #\n");
+  /*
+   block = new BlockModel(
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+   );
+   block = new BlockModel(
+       "##\n"
+       "# \n"
+   );
+   block= new BlockModel(
 " #  #\n"
 "######\n"
-    );
-    block = new BlockModel(
-        "#\n"
-    );
-    block = new BlockModel(
-        "#####\n"
-    );
-    block = new BlockModel(
-        " #\n"
-        "#\n"
-    );
-    block = new BlockModel(
-        "#\n"
-        "#\n"
-        "#\n"
-        "#\n"
-    );
-    block = new BlockModel(
-        "##\n"
-        "#\n"
-        "#\n"
-        "#\n"
-    );*/
-    
+   );
+   block = new BlockModel(
+       "#\n"
+   );
+   block = new BlockModel(
+       "#####\n"
+   );
+   block = new BlockModel(
+       " #\n"
+       "#\n"
+   );
+   block = new BlockModel(
+       "#\n"
+       "#\n"
+       "#\n"
+       "#\n"
+   );
+   block = new BlockModel(
+       "##\n"
+       "#\n"
+       "#\n"
+       "#\n"
+   );*/
+
   return 0;
 }
 
-BlockModel * GetRandomBlock() {
-    auto n=rand()%AbstractModel::list_bloks.size();
-    auto smth_elm =AbstractModel::list_bloks.begin();
-    for (size_t i = 0; i != n; ++i) {
-        ++smth_elm;
-    }
-    return (BlockModel *)smth_elm->second;
+BlockModel *GetRandomBlock() {
+  auto offset_n = rand() % AbstractModel::list_bloks.size();
+  auto smth_elm = AbstractModel::list_bloks.begin();
+
+  for (size_t cur_offset = 0; cur_offset != offset_n; ++cur_offset) {
+    ++smth_elm;
+  }
+
+  return (BlockModel *) smth_elm->second;
 }
-BlockModel* GetBlock(int n_elm) {
-    auto smth_elm = AbstractBlockModel::list_bloks.begin();
-    for (size_t i = 0; i != n_elm; ++i) {
-        ++smth_elm;
-    }
-    return (BlockModel*)smth_elm->second;
+BlockModel *GetBlock(int n_elm) {
+  auto smth_elm = AbstractBlockModel::list_bloks.begin();
+
+  for (size_t cur_offset = 0; cur_offset != n_elm; ++cur_offset) {
+    ++smth_elm;
+  }
+
+  return (BlockModel *) smth_elm->second;
 }
-int main()
-{
-    loadBloks();
-    srand(time(0));
-    AbstractView smth_view;
-    MapController* map = new MapController();
-    
-    MapModel* map_model = new MapModel();
-    map_model->SetModelSize(GlobalVars::x_size, GlobalVars::y_size);
-    map->SetModel(map_model);
-    
-    char ch;
-    
-    bool isCicle = true;
+int main() {
+  loadBloks();
+  srand(time(0));
+  AbstractView smth_view;
+  MapController *map = new MapController();
 
-    BlockModel* elm = 0;
-    elm = new BlockModel(*GetRandomBlock());
-    elm->SetParent(map_model);
-    map->AddSecondSmthElm(elm);
+  MapModel *map_model = new MapModel();
+  map_model->SetModelSize(GlobalVars::x_size, GlobalVars::y_size);
+  map->SetModel(map_model);
 
-    elm = new BlockModel(*GetRandomBlock());
-    elm->SetParent(map_model);
-    map->AddSecondSmthElm(elm);
-    GlobalVars::need_new_block = false;
-    map->StartGame();
-    while (true) {
-        ch=_getch();
-        switch(ch) {
-        case 'q': isCicle = false; break;
-        //case 'w': map->MoveAbove(); break;
-        case 'a': map->MoveLeft(); break;
-        case 's': map->MoveDown(); break;
-        case 'd': map->MoveRight(); break;
-        case 'w': map->ElmRotate(); break;
-        case 'r': {
-            delete map_model;
-            delete map;
-            map = new MapController();
-            map_model = new MapModel();
-            map_model->SetModelSize(GlobalVars::x_size, GlobalVars::y_size);
-            map->SetModel(map_model);
-            elm = new BlockModel(*((BlockModel*)GetBlock(0)));
-            elm->SetParent(map_model);
-            map->AddSecondSmthElm(elm);
+  char ch;
+  bool isCicle = true;
 
-            elm = new BlockModel(*GetRandomBlock());
-            elm->SetParent(map_model);
-            map->AddSecondSmthElm(elm);
-            GlobalVars::need_new_block = false;
-            map->StartGame();
-            break;
-        }
-        }
-        
+  BlockModel *elm = 0;
+  elm = new BlockModel(*GetRandomBlock());
+  elm->SetParent(map_model);
+  map->AddSecondSmthElm(elm);
+
+  elm = new BlockModel(*GetRandomBlock());
+  elm->SetParent(map_model);
+  map->AddSecondSmthElm(elm);
+  GlobalVars::need_new_block = false;
+  map->StartGame();
+
+  while (true) {
+    ch = _getch();
+    switch (ch) {
+    case 'q':
+      isCicle = false;
+      break;
+    // case 'w': map->MoveAbove(); break;
+    case 'a':
+      map->MoveLeft();
+      break;
+    case 's':
+      map->MoveDown();
+      break;
+    case 'd':
+      map->MoveRight();
+      break;
+    case 'w':
+      map->ElmRotate();
+      break;
+    case 'r': {
+      delete map_model;
+      delete map;
+      map = new MapController();
+      map_model = new MapModel();
+      map_model->SetModelSize(GlobalVars::x_size, GlobalVars::y_size);
+      map->SetModel(map_model);
+      elm = new BlockModel(*((BlockModel *) GetBlock(0)));
+      elm->SetParent(map_model);
+      map->AddSecondSmthElm(elm);
+
+      elm = new BlockModel(*GetRandomBlock());
+      elm->SetParent(map_model);
+      map->AddSecondSmthElm(elm);
+      GlobalVars::need_new_block = false;
+      map->StartGame();
+      break;
     }
-    delete elm;
-    system("pause");
+    }
+  }
+
+  delete elm;
+  
+  system("pause");
 }
